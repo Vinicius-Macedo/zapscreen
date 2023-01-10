708324ee3bdda5dfa5fc2240b7a3b29a9869f59f
vinicius-macedo <vinicius.silva1076@etec.sp.gov.br>
2023-01-09 22:02:14 -0300

    removed sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,15 @@
 import HeroSection from "../components/sections/HeroSection";
-import AboutMeSection from "../components/sections/AboutMeSection";
-import ServicesSection from "../components/sections/ServicesSection";
-import HardSkillsSection from "../components/sections/HardSkillsSection";
-import HowWeGonnaWorkSection from "../components/sections/HowWeGonnaWorkSection";
+
 
 export default function Home() {
   return (
     <>
       <main>
         <HeroSection />
-        <AboutMeSection />
-        <ServicesSection />
-        <HardSkillsSection />
-        <HowWeGonnaWorkSection />
+        {/* <AboutMeSection /> */}
+        {/* <ServicesSection /> */}
+        {/* <HardSkillsSection /> */}
+        {/* <HowWeGonnaWorkSection /> */}
       </main>
     </>
   );
